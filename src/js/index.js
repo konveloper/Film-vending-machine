23c13d9d0a98c0598809f03e7ec8fedc47e15447
konveloper <konveloper@gmail.com>
2023-10-23 00:35:16 +0900

    feat: 입금 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import FilmGenerator from './components/filmGenerator.js';
+import Vendingmachine from './components/vendingmachine.js';
 
 const filmGenerator = new FilmGenerator();
+const vendingmachine = new Vendingmachine();
 
 await filmGenerator.setup();
+vendingmachine.setup();
